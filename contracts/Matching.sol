b072efecaced4d05f64e6eac594cd4f772f16245
Deepal Tennakoon <deepal.tennakoon@igreendata.com.au>
2024-08-05 11:59:24 +1000

    updated

--- contracts/Matching.sol
+++ contracts/Matching.sol
@@ -3,20 +3,24 @@ pragma solidity ^0.8.24;
 
 contract Matching {
     // Storage for hashes
-    bytes32[10000] public hashes;
-    uint256 public count;
+ 
+    uint size;
+    uint256 public count = 0;
+    bytes32 [] hashes;
+    
     
     // Event to emit when a hash is added
     event HashAdded(bytes32 newHash);
 
     // Constructor sets the count value
-    constructor() {
-        count = 0;
+    constructor(uint input) {
+        size = input;
+        hashes = new bytes32[](size);
     }
 
     // Function to add a new hash
     function addHash(bytes32 newHash) public {
-        require(count < 10000, "Hash storage is full");
+        require(count < size, "Hash storage is full");
         // Add the new hash
         hashes[count] = newHash;
         count++;
@@ -24,13 +28,14 @@ contract Matching {
         emit HashAdded(newHash);
     }
 
+
     // Function to get the current count of stored hashes
     function getCount() public view returns (uint256) {
         return count;
     }
 
     function matchHash(bytes32 inputHash) public view returns (bool){
-        for(uint k = 0; k < 10000; k++){
+        for(uint k = 0; k < size; k++){
             if (hashes[k] == inputHash) {
                 return true;
             }
